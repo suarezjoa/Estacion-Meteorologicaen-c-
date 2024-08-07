f6ad4e9d96f57c3598704e6fe91c793060f9f4fc
ivany <ivany@DESKTOP-RQ68ITE>
2024-08-07 20:34:55 -0300

    Agregar archivos de proyecto.

---
+++ Estacion Meteorologica/Calendario.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacion_Meteorologica
+{
+    internal class Calendario
+    {
+        public float[,] tempDiarias { get; set; } = new float[5, 7];
+        public float[] tempPromedio { get; set; } = new float[5];
+        public List<float> tempUmbral { get; set; } = new List<float>();
+        public void CargaTempDias () // Funcion para añadir temperaturas diarias. 31 dias.
+        {
+            Random nram = new Random();
+            for (int semana = 0; semana < 5; semana++)
+            {
+                for (int dia = 0; dia < 7; dia++)
+                {
+                    tempDiarias[semana, dia] = (float)nram.NextDouble() * 50 - 10;
+                    // cargo temperaturas que van de -10 a 40 grados nomas.
+                }
+            }
+        }
+
+        public void CalcularPromediosSemanales() // Funcion para calccular las temperaturas promedio de cada semana y almacenarlas en una coleccion.
+        {
+            for (int semana = 0; semana < 5; semana++)
+            {
+                float sumaTemperaturas = 0;
+                for (int dia = 0; dia < 7; dia++)
+                {
+                    sumaTemperaturas += tempDiarias[semana, dia];
+                }
+                tempPromedio[semana] = sumaTemperaturas / 7;
+            }
+        }
+        public float CalcularPromedioMes() // Funcion para calcular el promedio de temperatura en el mes.
+        {
+            float prom = 0;
+            int div = 0;
+            foreach ( var promedio in tempPromedio)
+            {
+                div++;
+                prom = promedio + prom;
+            }
+            return prom = prom / div;
+        }
+        public void FiltrarTempPorUmbral() // Funcion para encontrar todas las temperaturas que son mayores de 20° y las guardamos en una coleccion.
+        {
+            tempUmbral.Clear();
+            // Limpio la lista por si ejecutamos varias veces el programa y bueno. Se limpien los datos en base lo que el usuario quiera.
+            for (int semana = 0; semana < 5; semana++)
+            {
+                for (int dia = 0; dia < 7; dia++)
+                {
+                    if (tempDiarias[semana, dia] > 20)
+                    {
+                        tempUmbral.Add(tempDiarias[semana, dia]);
+                    }
+                }
+            }
+        }
+        public float mostrarUnDiaPuntual(int semana, int dia)
+        {
+            return tempDiarias[semana, dia];
+        }
+        public void mostrarTODO()
+        {
+            for (int semana = 0; semana < 5; semana++)
+            {
+                for (int dia = 0; dia < 7; dia++)
+                {
+                        Console.WriteLine(tempDiarias[semana, dia]);
+                }
+            }
+        }
+        public void mostrarDiaPorDia()
+        {
+            int semana = 0;
+            int dia = 0;
+            Console.WriteLine($"Temperatura del día {dia}, semana {semana}: {tempDiarias[semana, dia]}");
+            do
+            {
+                Console.WriteLine("Desea ver la temperatura del siguiente día? (Presione 1 para sí, 0 para no)");
+                int opcion = int.Parse(Console.ReadLine());
+                if (opcion == 1)
+                {
+                    dia++;
+                    if (dia > 7)
+                    {
+                        dia = 0;
+                        semana++;
+                    }
+                }
+                else {break;}
+                if (semana > 5){break;}
+
+                Console.WriteLine($"Temperatura del día {dia}, semana {semana}: {tempDiarias[semana, dia]}");
+
+                if (dia == 0)
+                {
+                    Console.WriteLine("Desea ver la temperatura de la siguiente semana? (Presione 1 para sí, 0 para no)");
+                    opcion = int.Parse(Console.ReadLine());
+
+                    if (opcion == 0)
+                    {
+                        break;
+                    }
+                }
+            } while (true);
+        }
+        public void BuscarTemperaturasExtremas() //Funcion para buscar las temperaturas mas altas y bajas del mes.
+        {
+            var temperaturas = tempDiarias.Cast<float>();
+
+            float tempMax = temperaturas.Max();
+            float tempMin = temperaturas.Min();
+
+            Console.WriteLine($"La temperatura más alta del mes es: {tempMax}");
+            Console.WriteLine($"La temperatura más baja del mes es: {tempMin}");
+        }
+        public void mostrarTodosLosUmbral()
+        {
+            foreach (var umbral in tempUmbral )
+            {
+                Console.WriteLine(umbral);
+            }
+        }
+    }
+}
---
+++ Estacion Meteorologica/Program.cs
@@ -0,0 +1,75 @@
+﻿
+using Estacion_Meteorologica;
+using System.Globalization;
+using System.Linq.Expressions;
+
+Calendario calendario1 = new Calendario();
+calendario1.CargaTempDias();
+calendario1.CalcularPromediosSemanales();
+calendario1.FiltrarTempPorUmbral();
+int cierre = 0;
+do
+{
+    Console.WriteLine("Elija entre una de las siguientes opciones:");
+    Console.WriteLine("1 = Ver la temperatura que hizo en un dia especifico del mes");
+    Console.WriteLine("2 = Ver una por una las temperaturas que hizo en mes( en base lo que desea el usuario ) ");
+    Console.WriteLine("3 = Ver el promedio de temperaturas del mes");
+    Console.WriteLine("4 = Ver el promedio de temperaturas de las semanas");
+    Console.WriteLine("5 = Ver la temperatura mas alta y baja que hizo en el mes");
+    Console.WriteLine("6 = Ver temperaturas que son superiores a el umbral de 20°");
+    int expresion = int.Parse(Console.ReadLine());
+
+    switch (expresion)
+    {
+        case 1:
+            Console.WriteLine("ingrese el numero de la semana (1,2,3,4)");
+            int semana = int.Parse(Console.ReadLine());
+            semana = semana - 1;
+            Console.WriteLine("ingrese el numero del dia de la semana (1,2,3,4,5,6,7)");
+            int dia = int.Parse(Console.ReadLine());
+            dia = dia - 1;
+            Console.WriteLine("la temperatura fue de : " + calendario1.mostrarUnDiaPuntual(semana, dia));
+            if (calendario1.mostrarUnDiaPuntual(semana, dia) < 0)
+            {
+                Console.WriteLine("Hizo mucho frio.");
+            }
+            // la consig deja en claro que pasa cuando es 0 o 20. Aca aplico la solucion a ese posible error.
+            else if (calendario1.mostrarUnDiaPuntual(semana, dia) >= 0 && calendario1.mostrarUnDiaPuntual(semana, dia) <= 20)
+            {
+                Console.WriteLine("El clima estaba fresco");
+            }
+            else { Console.WriteLine("Hizo calor afuera"); }
+            break;
+
+        case 2:
+            calendario1.mostrarDiaPorDia();
+            break;
+
+        case 3:
+            Console.WriteLine(calendario1.CalcularPromedioMes());
+            break;
+        case 4:
+            for (int i = 0; i < calendario1.tempPromedio.Length; i++)
+            {
+                Console.WriteLine($"Promedio de la semana {i}: {calendario1.tempPromedio[i]}");
+            }
+            break;
+        case 5:
+            {
+                calendario1.BuscarTemperaturasExtremas();
+                break;
+            }
+        case 6:
+            {
+                calendario1.mostrarTodosLosUmbral();
+                break;
+            }
+
+        default:
+            break;
+    }
+    Console.WriteLine("Desea selecionar otra opcion ? presione 0, si no 1");
+    cierre = int.Parse(Console.ReadLine());
+} while (cierre == 1);
+Console.WriteLine("Muchas gracias");
+
